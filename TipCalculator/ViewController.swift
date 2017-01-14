090280f1c11243c8bea627ca2c7cb504097ae7e4
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2017-01-14 15:34:56 -0500

    TipCalculator 1.0  2017/01/14

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var tipControl: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBAction func onTap(_ sender: Any)
+    {
+        view.endEditing(true);
+    }
+    @IBAction func calTip(_ sender: AnyObject)
+    {
+        let tipPercent = [0.18,0.2,0.25];
+        
+        let bill = Double(billField.text!) ?? 0;
+        let tip = bill*tipPercent[tipControl.selectedSegmentIndex];
+        let total = bill + tip;
+        
+        tipLabel.text = String(format: "$%.2f", tip);
+        totalLabel.text = String(format: "$%.2f", total);
+        
+        
+        
+    }
 }
 
